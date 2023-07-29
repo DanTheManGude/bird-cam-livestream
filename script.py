c5de434229f70131e9dde69114e32664567d8fd8
DanTheManGude <dgude31@gmail.com>
2023-07-29 19:53:06 -0400

    Get videos from my channel

--- script.py
+++ script.py
@@ -1,8 +1,28 @@
 import os
 from dotenv import load_dotenv
+import json
+import googleapiclient.discovery
 
 load_dotenv()
 
+api_service_name = "youtube"
+api_version = "v3"
 DEVELOPER_KEY = os.getenv("DEVELOPER_KEY")
 
-print(DEVELOPER_KEY)
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=DEVELOPER_KEY
+)
+
+
+request = youtube.search().list(
+    part="id,snippet",
+    type="video",
+    maxResults=5,
+    fields="items(id(videoId),snippet(publishedAt,title))",
+    channelId="UC7orL7AlwFoMYNnA-YURoBA",
+)
+response = request.execute()
+
+json_formatted_str = json.dumps(response, indent=2)
+
+print(json_formatted_str)
